ec95aa9647dca4d14ef5f8d33f2281c587d9424b
Name <namesexistsinemails@gmail.com>
2024-10-16 16:40:59 +0100

    Dev: Switch from arena tree to ZC* parser
    It does technically make copies of the data but i'm trying to minimise
    this to one or two instances.

--- src/main.rs
+++ src/main.rs
@@ -2,160 +2,12 @@ use std::collections::HashMap;
 
 mod vel {
     use core::fmt;
-    use std::{
-        collections::{HashMap, HashSet},
-        fmt::Display,
-    };
-
-    #[derive(Debug)]
-    pub struct ArenaTree<T> {
-        arena: Vec<Node<T>>,
-        dead_nodes: Vec<usize>,
-    }
-
-    impl<T> ArenaTree<T> {
-        fn new() -> Self {
-            ArenaTree {
-                arena: vec![],
-                dead_nodes: vec![],
-            }
-        }
-
-        fn add_node(&mut self, parent: Option<usize>, data: T) -> usize {
-            if let Some(node_index) = self.dead_nodes.pop() {
-                let node = &mut self.arena[node_index];
-                node.parent = parent;
-                node.data = Some(data);
-                node_index
-            } else {
-                let index = self.arena.len();
-                if let Some(parent_index) = parent {
-                    self.arena[parent_index].add_child(index);
-                }
-                self.arena.push(Node::new(index, parent, data));
-                index
-            }
-        }
-
-        fn remove_node(&mut self, index: usize) {
-            self.dead_nodes.push(index);
-            let node = &mut self.arena[index];
-            node.data = None;
-
-            let mut stack: Vec<usize> = node.children.drain(..).collect();
-            if let Some(parent_index) = node.parent {
-                self.arena[parent_index].delete_child(index)
-            }
-
-            while let Some(index) = stack.pop() {
-                self.dead_nodes.push(index);
-                let node = &mut self.arena[index];
-                node.data = None;
-                stack.extend(node.children.drain(..));
-            }
-        }
-    }
-
-    /// If data is None then that means that this Node is dead and can be safely removed or
-    /// repurposed.
-    #[derive(Debug)]
-    struct Node<T> {
-        index: usize,
-        parent: Option<usize>,
-        children: Vec<usize>,
-        data: Option<T>,
-    }
-
-    impl<T> Node<T> {
-        fn new(index: usize, parent: Option<usize>, data: T) -> Self {
-            Node {
-                index,
-                parent,
-                children: vec![],
-                data: Some(data),
-            }
-        }
-
-        fn add_child(&mut self, child: usize) {
-            self.children.push(child);
-        }
-
-        fn delete_child(&mut self, child: usize) {
-            self.children.retain(|&stored_child| stored_child != child)
-        }
-    }
-
-    /// The Tag of the element is always the first element in the attributes vector.
-    ///
-    /// If the Tag contains a empty string then this means it is text element.
-    #[derive(Debug)]
-    pub struct DOMElement<'a> {
-        void_element: bool,
-        attributes: Vec<ElementAttributes<'a>>,
-        text: &'a str,
-    }
-
-    impl<'a> DOMElement<'a> {
-        /// The Tag field MUST be ElementAttributes::Tag()
-        fn new(
-            tag: ElementAttributes<'a>,
-            mut attributes: Vec<ElementAttributes<'a>>,
-            text: &'a str,
-            void_element: bool,
-        ) -> Self {
-            if let ElementAttributes::Tag(_) = tag {
-                attributes.insert(0, tag);
-                DOMElement {
-                    void_element,
-                    attributes,
-                    text,
-                }
-            } else {
-                panic!("Tag field can only be ElementAttributes::Tag()!")
-            }
-        }
-    }
-
-    #[derive(Debug)]
-    pub enum ElementAttributes<'a> {
-        /// Represents an HTML tag (e.g. "div", "p")
-        Tag(&'a str),
-        /// Represents an element's ID (e.g. "header")
-        Id(&'a str),
-        /// Represents an element's class (e.g. "navbar")
-        Class(&'a str),
-        /// Represents any other attribute (name, value)
-        Other(&'a str, &'a str),
-    }
-
-    /// Test
-    #[derive(Debug)]
-    pub enum EditableField {
-        /// Specifies the nodes text itself is editable
-        Text,
-        /// Specifies the nodes which node attribute is editable
-        Attribute(usize),
-    }
-
-    pub type DOM<'a> = ArenaTree<DOMElement<'a>>;
-
-    #[derive(Debug)]
-    pub struct VelDOM<'a> {
-        /// This contains an array of all the nodes which can be substitued for new text
-        editable_nodes: Vec<(usize, EditableField)>,
-        /// The underlying DOM
-        dom: DOM<'a>,
-    }
-
-    impl<'a> VelDOM<'a> {
-        pub fn render(ignore: HashSet<Vec<ElementAttributes>>) {
-            todo!()
-        }
-    }
+    use std::{collections::HashMap, fmt::Display, thread::panicking};
 
     #[derive(Debug)]
     pub struct VelInstance<'a> {
         components: HashMap<&'a str, &'a str>,
+        cache: HashMap<&'a str, String>,
     }
 
     impl Display for VelInstance<'_> {
@@ -166,7 +18,10 @@ mod vel {
 
     impl<'a> VelInstance<'a> {
         pub fn new(components: HashMap<&'a str, &'a str>) -> Self {
-            Self { components }
+            Self {
+                components,
+                cache: HashMap::new(),
+            }
         }
 
         pub fn extend(&mut self, components: HashMap<&'a str, &'a str>) -> &mut Self {
@@ -174,33 +29,174 @@ mod vel {
             self
         }
 
-        pub fn parse(
-            &'a self,
+        pub fn render<'b>(
+            &'b mut self,
             component: &'a str,
-            inputs: HashMap<&str, &str>,
+            inputs: HashMap<&'a str, &'a str>,
             //) -> Result<&Self, Err> {
-        ) -> VelDOM {
-            println!("{}", component);
-            dbg!(&self.components);
-            let _page = self
-                .components
-                .get(component)
-                .expect("could get the component numbskull");
-            let mut dom = VelDOM::new();
+        ) -> String {
+            fn parse_variable(input: &str) -> Option<(usize, &str)> {
+                for (index, char) in input.char_indices() {
+                    if char == '}' {
+                        return Some((index + 2, &input[..index].trim()));
+                    } else if !char.is_alphabetic() && char != ' ' {
+                        println!("{}", char);
+                        break;
+                    };
+                }
+                return None;
+            }
 
-            dom
+            fn parse_component<'b>(
+                input: &str,
+                components: &HashMap<&'b str, &'b str>,
+                inputs: HashMap<&'b str, &'b str>,
+            ) -> Option<(usize, &'b str, HashMap<&'b str, &'b str>)> {
+                let mut chars = input.char_indices();
+
+                if let Some((_, char)) = chars.next() {
+                    if char.is_lowercase() {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+
+                if let Some((name_end_pos, _)) = chars
+                    .clone()
+                    .skip_while(|(_, char)| char.is_alphanumeric())
+                    .next()
+                {
+                    let mut end_pos = 0;
+                    let mut params: HashMap<&str, &str> = HashMap::new();
+
+                    let mut name_start_index = 0; // For attribute names
+                    let mut value_start_index = 0; // For attribute values
+                    let mut in_quotes = false; // Track whether we're in quotes
+
+                    for (index, char) in chars {
+                        match char {
+                            '/' | '>' => {
+                                // End of component detected
+                                if !in_quotes {
+                                    end_pos = index;
+                                    break;
+                                }
+                            }
+                            '"' | '\'' => {
+                                // Toggle in_quotes state
+                                if in_quotes {
+                                    // Ending the value
+                                    let key = &input[name_start_index..index].trim();
+                                    let value = &input[value_start_index..index].trim();
+                                    params.insert(key, value);
+                                    value_start_index = 0; // Reset for the next attribute
+                                    in_quotes = false; // Reset quotes
+                                } else {
+                                    // Starting a new quoted value
+                                    value_start_index = index + 1; // Move past the quote
+                                    in_quotes = true;
+                                }
+                            }
+                            ' ' => {
+                                // Handle spaces
+                                if !in_quotes {
+                                    if name_start_index != 0 {
+                                        // If we finished reading an attribute name
+                                        let key = &input[name_start_index..index].trim();
+                                        // If no value was provided, treat it as a boolean attribute
+                                        params.insert(key, key);
+                                        name_start_index = 0; // Reset for the next attribute
+                                    }
+                                }
+                            }
+                            char => {
+                                // Capture alphanumeric characters for attribute names
+                                if char.is_alphanumeric() {
+                                    if name_start_index == 0 {
+                                        name_start_index = index; // Start of an attribute name
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if let Some(name) = components.get(&input[..name_end_pos]) {
+                        return Some((end_pos, name.clone(), inputs));
+                    }
+                }
+
+                return None;
+            }
+
+            // HACK: Lazy fuck couldnt make a proper implementation because they suck ass at
+            // borrowing. FIX THIS!!
+            return if let Some(cached_page) = self.cache.get(component) {
+                cached_page.to_string()
+            } else if let Some(page) = self.components.get(component) {
+                let mut page = page.to_string();
+                let mut last_pos = 0;
+                let mut cachable = true;
+
+                while last_pos + 1 < page.len() {
+                    match page[last_pos..last_pos + 1].chars().next().unwrap() {
+                        '{' => {
+                            cachable = false;
+                            if let Some((close_brack_pos, var_name)) =
+                                parse_variable(&page[last_pos + 1..])
+                            {
+                                if let Some(value) = &inputs.get(var_name) {
+                                    page.replace_range(last_pos..last_pos + close_brack_pos, value);
+                                    last_pos += 1 + value.len()
+                                } else {
+                                    last_pos += 1 + close_brack_pos
+                                }
+                            } else {
+                                last_pos += 1
+                            }
+                        }
+                        '<' => {
+                            cachable = false;
+                            if let Some((index, component, inputs)) = parse_component(
+                                &page[last_pos + 1..],
+                                &self.components,
+                                inputs.clone(),
+                            ) {
+                                let _ = self.render(component, inputs);
+                            } else {
+                                last_pos += 1
+                            }
+                        }
+                        _ => last_pos += 1,
+                    }
+                    if cachable {
+                        self.components.remove(component);
+                        self.cache.insert(component, page.clone());
+                    }
+                }
+                page
+            } else {
+                eprintln!(
+                    "components: {:?}, cache: {:?}, input: {:?}",
+                    self.components, self.cache, component
+                );
+                panic!("This is a very bad situation.")
+            };
         }
     }
 }
 
-use crate::vel::{ArenaTree, DOMElement, VelInstance};
+use crate::vel::VelInstance;
 
 fn main() {
-    let test = VelInstance::new(HashMap::from([("Component", "banana")]));
-    dbg!(&test);
-    let test_dom = test.parse("Component", HashMap::from([("data", "cheese")]));
-    dbg!(&test_dom);
+    let mut test = VelInstance::new(HashMap::from([
+        ("Component", "banana {data} {data} {data} <Ab/>"),
+        ("Ab", "Bollocks"),
+    ]));
+    //dbg!(&test);
+    let test_output = test.render("Component", HashMap::from([("data", "cheese")]));
+    dbg!(&test_output);
 }
 
-// * Read file -> Tree -> Render
-// Take arguements -> Parse file -> Return html
+//  Read file -> Tree -> Render
+// * Take arguements -> Parse file -> Return html

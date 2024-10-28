4a6ab635514eabc500a650a00067555cda67726f
Name <lasagna@garfunkles.space>
2024-10-28 02:13:57 +0000

    Feat: GET TO A SOMEWHAT WORKING VERSION!!

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 
 mod vel {
     use core::fmt;
-    use std::{collections::HashMap, fmt::Display, thread::panicking};
+    use std::{collections::HashMap, fmt::Display, str::Chars};
 
     #[derive(Debug)]
     pub struct VelInstance<'a> {
         components: HashMap<&'a str, &'a str>,
-        cache: HashMap<&'a str, String>,
+        cache: HashMap<String, String>,
     }
 
     impl Display for VelInstance<'_> {
@@ -29,152 +29,171 @@ mod vel {
             self
         }
 
-        pub fn render<'b>(
-            &'b mut self,
-            component: &'a str,
-            inputs: HashMap<&'a str, &'a str>,
+        pub fn render(
+            &'_ mut self,
+            component: String,
+            inputs: HashMap<String, String>,
             //) -> Result<&Self, Err> {
         ) -> String {
-            fn parse_variable(input: &str) -> Option<(usize, &str)> {
-                for (index, char) in input.char_indices() {
-                    if char == '}' {
-                        return Some((index + 2, &input[..index].trim()));
-                    } else if !char.is_alphabetic() && char != ' ' {
-                        println!("{}", char);
-                        break;
-                    };
-                }
-                return None;
+            fn parse_variable(
+                char_iterator: &mut Chars,
+                inputs: &HashMap<String, String>,
+            ) -> String {
+                let variable_name: String = char_iterator.take_while(|char| char != &'}').collect();
+                inputs
+                    .get(variable_name.as_str())
+                    .map(|value| value.to_string())
+                    .unwrap_or_else(|| format!("{{{}}}", variable_name))
             }
 
-            fn parse_component<'b>(
-                input: &str,
-                components: &HashMap<&'b str, &'b str>,
-                inputs: HashMap<&'b str, &'b str>,
-            ) -> Option<(usize, &'b str, HashMap<&'b str, &'b str>)> {
-                let mut chars = input.char_indices();
+            fn parse_tags(
+                parent: &mut VelInstance<'_>,
+                char_iterator: &mut Chars,
+                inputs: &HashMap<String, String>,
+            ) -> String {
+                if let Some(first_char) = char_iterator.next() {
+                    if first_char.is_uppercase() {
+                        // State vars
+                        let mut in_quotes = false;
 
-                if let Some((_, char)) = chars.next() {
-                    if char.is_lowercase() {
-                        return None;
-                    }
-                } else {
-                    return None;
-                }
+                        let mut buffer = String::from(first_char);
+                        // The end of each element is specified by an empty character in the array.
+                        // CAN BE CHANGED IN FUTURE
+                        let mut attributes: Vec<String> = vec![];
 
-                if let Some((name_end_pos, _)) = chars
-                    .clone()
-                    .skip_while(|(_, char)| char.is_alphanumeric())
-                    .next()
-                {
-                    let mut end_pos = 0;
-                    let mut params: HashMap<&str, &str> = HashMap::new();
+                        while let Some(char) = char_iterator.next() {
+                            /*
+                            dbg!(&char);
+                            dbg!(&buffer);
+                            dbg!(&attributes);
+                            */
 
-                    let mut name_start_index = 0; // For attribute names
-                    let mut value_start_index = 0; // For attribute values
-                    let mut in_quotes = false; // Track whether we're in quotes
-
-                    for (index, char) in chars {
-                        match char {
-                            '/' | '>' => {
-                                // End of component detected
-                                if !in_quotes {
-                                    end_pos = index;
-                                    break;
-                                }
-                            }
-                            '"' | '\'' => {
-                                // Toggle in_quotes state
-                                if in_quotes {
-                                    // Ending the value
-                                    let key = &input[name_start_index..index].trim();
-                                    let value = &input[value_start_index..index].trim();
-                                    params.insert(key, value);
-                                    value_start_index = 0; // Reset for the next attribute
-                                    in_quotes = false; // Reset quotes
-                                } else {
-                                    // Starting a new quoted value
-                                    value_start_index = index + 1; // Move past the quote
-                                    in_quotes = true;
-                                }
-                            }
-                            ' ' => {
-                                // Handle spaces
-                                if !in_quotes {
-                                    if name_start_index != 0 {
-                                        // If we finished reading an attribute name
-                                        let key = &input[name_start_index..index].trim();
-                                        // If no value was provided, treat it as a boolean attribute
-                                        params.insert(key, key);
-                                        name_start_index = 0; // Reset for the next attribute
+                            // NOTE: Whilst this is more complex than it needs to be, it gives us
+                            // the opportunity to do things like Svelte's slot in future.
+                            match char {
+                                '"' | '\'' => in_quotes = !in_quotes,
+                                ' ' | '>' => {
+                                    if !in_quotes {
+                                        attributes.push(buffer.clone());
+                                        buffer.clear()
                                     }
                                 }
-                            }
-                            char => {
-                                // Capture alphanumeric characters for attribute names
-                                if char.is_alphanumeric() {
-                                    if name_start_index == 0 {
-                                        name_start_index = index; // Start of an attribute name
+                                '{' => buffer.push_str(&parse_variable(char_iterator, inputs)),
+                                '<' => {
+                                    if !in_quotes {
+                                        attributes.push(buffer.clone());
+                                        buffer.clear();
+
+                                        let tag_value = parse_tags(parent, char_iterator, inputs);
+                                        if tag_value.contains(format!("/{}", first_char).as_str()) {
+                                            break;
+                                        } else {
+                                            attributes.push(tag_value);
+                                            buffer.clear();
+                                        }
                                     }
                                 }
+                                other_char => buffer.push(other_char),
                             }
                         }
-                    }
 
-                    if let Some(name) = components.get(&input[..name_end_pos]) {
-                        return Some((end_pos, name.clone(), inputs));
+                        let mut inputs = inputs.clone();
+                        inputs.extend(
+                            attributes
+                                .iter()
+                                .skip(1)
+                                .take_while(|attribute| !attribute.is_empty())
+                                .map(|attribute| {
+                                    let (key, value) =
+                                        attribute.split_once('=').unwrap_or((attribute, attribute));
+                                    (key.to_owned(), value.to_owned())
+                                }),
+                        );
+
+                        parent.render(attributes[0].to_owned(), inputs)
+                    } else {
+                        format!(
+                            "<{}{}>",
+                            first_char,
+                            char_iterator
+                                .take_while(|char| char != &'>')
+                                .collect::<String>()
+                        )
                     }
+                } else {
+                    "<".to_string()
                 }
-
-                return None;
             }
 
-            // HACK: Lazy fuck couldnt make a proper implementation because they suck ass at
-            // borrowing. FIX THIS!!
-            return if let Some(cached_page) = self.cache.get(component) {
+            return if let Some(cached_page) = self.cache.get(component.as_str()) {
                 cached_page.to_string()
-            } else if let Some(page) = self.components.get(component) {
-                let mut page = page.to_string();
-                let mut last_pos = 0;
+            } else if let Some(page) = self.components.get(component.as_str()) {
+                let mut return_page = String::with_capacity(page.len());
+                let mut char_iterator = page.chars();
                 let mut cachable = true;
 
-                while last_pos + 1 < page.len() {
-                    match page[last_pos..last_pos + 1].chars().next().unwrap() {
+                while let Some(char) = char_iterator.next() {
+                    match char {
                         '{' => {
                             cachable = false;
-                            if let Some((close_brack_pos, var_name)) =
-                                parse_variable(&page[last_pos + 1..])
-                            {
-                                if let Some(value) = &inputs.get(var_name) {
-                                    page.replace_range(last_pos..last_pos + close_brack_pos, value);
-                                    last_pos += 1 + value.len()
-                                } else {
-                                    last_pos += 1 + close_brack_pos
-                                }
-                            } else {
-                                last_pos += 1
-                            }
+                            let variable_data = parse_variable(&mut char_iterator, &inputs);
+                            return_page.push_str(&variable_data);
                         }
                         '<' => {
                             cachable = false;
-                            if let Some((index, component, inputs)) = parse_component(
-                                &page[last_pos + 1..],
-                                &self.components,
-                                inputs.clone(),
-                            ) {
-                                let _ = self.render(component, inputs);
-                            } else {
-                                last_pos += 1
+                            let element_data = parse_tags(self, &mut char_iterator, &inputs);
+                            return_page.push_str(&element_data);
+                            /*
+                            // <Component vx-param="data" lang="fr" disabled/>
+                            // <p>This is some text</p>
+                            // <img src={image}/>
+                            let mut in_quotes = false;
+                            let mut last_pos = index + 1;
+                            let mut attributes = vec![];
+                            let mut slot: String;
+
+                            // TODO: Split this into a function for recursion
+                            while let Some((element_index, element_char)) = char_iterator.next() {
+                                println!("index: {}, char: {}", element_index, element_char);
+                                match element_char {
+                                    '"' | '\'' => in_quotes = !in_quotes,
+                                    ' ' => {
+                                        if !in_quotes {
+                                            dbg!(last_pos..element_index - 1);
+                                            attributes.push(&page[last_pos..element_index]);
+                                            last_pos = element_index + 1;
+                                        }
+                                    }
+                                    '/' => {
+                                        if !in_quotes {
+                                            attributes.push(&page[last_pos..element_index - 1]);
+                                            last_pos = element_index + 1;
+                                            break;
+                                        }
+                                    }
+                                    '{' => {parse_variable(
+                                        char_iterator,
+                                        &inputs
+                                    );},
+                                    '<' => todo!(), //NOTE: HERE WOULD BE WHERE YOU RECURSE
+                                    _ => (),
+                                }
                             }
+
+                            if attributes[0].starts_with(|char: char| char.is_uppercase()) {
+                                render_stack.push((attributes[0], inputs.clone()));
+                            }
+                            dbg!(attributes);
+                            */
                         }
-                        _ => last_pos += 1,
-                    }
-                    if cachable {
-                        self.components.remove(component);
-                        self.cache.insert(component, page.clone());
+                        other_char => return_page.push(other_char),
                     }
                 }
-                page
+                if cachable {
+                    self.components.remove(component.as_str());
+                    self.cache.insert(component, return_page.clone());
+                }
+                return_page
             } else {
                 eprintln!(
                     "components: {:?}, cache: {:?}, input: {:?}",
@@ -190,11 +209,20 @@ use crate::vel::VelInstance;
 
 fn main() {
     let mut test = VelInstance::new(HashMap::from([
-        ("Component", "banana {data} {data} {data} <Ab/>"),
-        ("Ab", "Bollocks"),
+        (
+            "Component",
+            "<p>hello, im {data}. Thats {alt} and they love to {something}.</p> <Ab hello test='{alt}'><Ab></Ab><p>test</p><p>test</p></Ab>"
+        ),
+        ("Ab", "<p>{hello}</p><img src=\"/img.png\""),
     ]));
     //dbg!(&test);
-    let test_output = test.render("Component", HashMap::from([("data", "cheese")]));
+    let test_output = test.render(
+        "Component".to_owned(),
+        HashMap::from([
+            ("data".to_owned(), "cheese".to_owned()),
+            ("alt".to_owned(), "alt thing".to_owned()),
+        ]),
+    );
     dbg!(&test_output);
 }
 

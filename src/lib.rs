3a2b4619f906a63349a260383a347d2e8c2751a6
Name <lasagna@garfunkles.space>
2024-12-31 19:11:21 +0000

    Feat: Move tests into a separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod tests;
+
 use std::{
     collections::HashMap,
     error,
@@ -225,14 +227,14 @@ fn parse_tags(char_iterator: &mut Chars, inputs: &HashMap<String, String>) -> Ve
                     collected_parts.push(buffer.clone());
                     buffer.clear()
                 }
+                // NOTE: an error could be thrown here in a stricter parsing mode if this isnt present
                 '>' if !in_quotes => {
                     if !buffer.is_empty() {
                         collected_parts.push(buffer.clone());
                         buffer.clear();
                     }
                     break;
-                } // NOTE: an error could be thrown here in a stricter parsing mode if this isnt
-                // present
+                }
                 '{' => buffer.push_str(&parse_variable(char_iterator, inputs)),
                 other_char => buffer.push(other_char),
             }
@@ -389,59 +391,3 @@ where
 
 //   Read file -> Tree -> Render
 // * Take arguements -> Parse file -> Return html
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    //fn render_only<'a>() -> Result<(), RenderingError<'a>> {
-    fn render_only<'a>() -> Result<(), RenderingError> {
-        let test_instance = VelInstance::new(HashMap::from([(
-            "Component".to_string(),
-            r#"
-        <div test>
-            <span style='color: red'>this is the inside text</span>, I sure do hope nothing happens to it
-        </div>
-        <p>hello, world</p>"#.to_string(),
-        )]));
-
-        let result = test_instance.render("Component".to_string(), HashMap::new(), |element| {
-            Some(element)
-        })?;
-        println!("render_only result: {}", result.trim());
-        assert_eq!(
-            result.trim(),
-            r#"<div test="test">
-            <span style="color: red">this is the inside text</span>, I sure do hope nothing happens to it
-        </div>
-        <p>hello, world</p>"#,
-        );
-
-        Ok(())
-    }
-
-    #[test]
-    //fn render_and_filter<'a>() -> Result<(), RenderingError<'a>> {
-    fn render_and_filter<'a>() -> Result<(), RenderingError> {
-        let test_instance = VelInstance::new(HashMap::from([(
-            "Component".to_string(),
-            r#"
-        <div test>
-            <span style='color: red'>this is the inside text</span>, I sure do hope nothing happens to it
-        </div>
-        <p>hello, world</p>"#.to_string(),
-        )]));
-
-        let result = test_instance.render("Component".to_string(), HashMap::new(), |element| {
-            if element.name != "div".to_string() {
-                return Some(element);
-            }
-            None
-        })?;
-
-        println!("render_and_filter result: {}", result.trim());
-        assert_eq!(result.trim(), "<p>hello, world</p>");
-        Ok(())
-    }
-}
